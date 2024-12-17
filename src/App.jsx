d0694adc05caa4081e55794d888461f6a80c51b7
Shekhar969 <shekharrawal96@gmail.com>
2024-12-17 22:03:28 +0545

    subject added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,40 @@
 import snscLogo from "./assets/logo.png";
 import "./App.css";
 
+const studentsName =[
+  {
+    Roll_NO:1,
+    name:"AAyush",
+  },
+  {
+    Roll_NO:2,
+    name:"Elisha",
+  },
+  {
+    Roll_NO:3,
+    name:"Roshan",
+  },
+  {
+    Roll_NO:4,
+    name:"Hairsh",
+  },
+  {
+    Roll_NO:5,
+    name:"Setal",
+  },
+]
+
 function App() {
   return (
     <>
       <img src={snscLogo} className="snscLogo" alt="SnscLogo" />
       <div className="subjects">
-        <a href="" className="subject" id="firstSubject"> <span>Cryptography</span></a>
-        <a href="" className="subject" id="secondSubject"> <span>Web Technology</span> </a>
-        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
-        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
-        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
-        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
+        <a href="" className="subject" id="firstSubject"> <h3>Cryptography</h3> <h4>sir</h4> </a>
+        <a href="" className="subject" id="firstSubject"> <h3>Cryptography</h3> <h4>sir</h4> </a>
+        <a href="" className="subject" id="firstSubject"> <h3>Cryptography</h3> <h4>sir</h4> </a>
+        <a href="" className="subject" id="firstSubject"> <h3>Cryptography</h3> <h4>sir</h4> </a>
+        <a href="" className="subject" id="firstSubject"> <h3>Cryptography</h3> <h4>sir</h4> </a>
+        <a href="" className="subject" id="firstSubject"> <h3>Cryptography</h3> <h4>sir</h4> </a>
       </div>
     </>
   );
