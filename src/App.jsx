8c596caf8a7c322c5022e052eafac09ccba29990
Shekhar969 <shekharrawal96@gmail.com>
2024-12-17 19:22:55 +0545

    basic Structure of the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,20 @@
+import snscLogo from "./assets/logo.png";
+import "./App.css";
 
+function App() {
+  return (
+    <>
+      <img src={snscLogo} className="snscLogo" alt="SnscLogo" />
+      <div className="subjects">
+        <a href="" className="subject" id="firstSubject"> <span>Cryptography</span></a>
+        <a href="" className="subject" id="secondSubject"> <span>Web Technology</span> </a>
+        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
+        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
+        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
+        <a href="" className="subject" id="thirdSubject"> <span>Wireless Networking</span> </a>
+      </div>
+    </>
+  );
+}
+
+export default App;
