bf9f5d879f98a8eaaae5e31e79b78b4d8858529c
Shekhar969 <shekharrawal96@gmail.com>
2024-12-18 22:41:23 +0545

    change in style of taking attendance

--- src/Components/Simulation-and-Modeling.jsx
+++ src/Components/Simulation-and-Modeling.jsx
@@ -1,12 +1,73 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import StudentsData from "./student-details"
+import { useState } from 'react'
 
-const SM = () => {
+  const SM = () => {
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const Attendance ="";
+    const currentStudent = StudentsData[currentIndex];
+  
+    const isPresent = () => {
+      if (currentIndex < StudentsData.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+        Attendance =true;
+      } else {
+        setCurrentIndex(0); 
+      }
+    };
+    const isAbsent = () => {
+      if (currentIndex < StudentsData.length - 1) {
+        setCurrentIndex(currentIndex + 1);
+        Attendance =false;
+      } else {
+        setCurrentIndex(0); 
+      }
+    };
   return (
-    <main>
-    hiiiiiiiiiiiiiii
-    <Link to="/"><button type="button" className="back-button"> Back </button></Link>
-</main>
+
+    <>
+      <Link to="/">
+        <button type="button" className="back-button">
+          Back
+        </button>
+      </Link>
+      <main className="particularSubject">
+        <div className="student-list">
+          {StudentsData.map((student, index) => (
+            <div key={student.rollno} className="student-item">
+              <span className="student-name">{index + 1}. {student.name}</span> 
+            </div>
+          ))}
+        </div>
+        <div className="eachStudent">
+        
+          <span className="serial-number">{currentIndex + 1}. </span> <br />
+        
+        <div className="student-item">
+          <img
+              className="student-photo"
+              src={StudentsData[currentIndex].imgSrc}
+              alt={StudentsData[currentIndex].name}
+            />
+          <div className="student-details">
+            <p>Name: {currentStudent.name}</p>
+            <p>Roll No: {currentStudent.rollno}</p>
+            <p>Address: {currentStudent.address}</p>
+          </div>
+        </div>
+
+        <button className="buttonAbs" onClick={isAbsent}>
+          Absent
+        </button>
+        <button className="buttonPre" onClick={isPresent}>
+          Present
+        </button>
+
+        </div>
+      </main>
+    </>
+
   )
 }
 
--- src/Components/student-details.jsx
+++ src/Components/student-details.jsx
@@ -1,19 +1,21 @@
-
 const StudentsData = [
-    {name: "Sakshi Khatiwada", 
+  { name: "Sakshi Khatiwada", 
     rollno: 29, 
-    address: "Bankhet"
-},
-    {
-        name: "Shekhar Rawal",
-        rollno: 32,
-        address: "Bhashi"
-    },
-    {
-        name: "Yojana Pandey",
-        rollno: 38,
-        address: "Bhashi"
-    },
-]
+    address: "Bankhet",
+    imgSrc: "./src/assets/sakshi.png"
+  },
+  {
+    name: "Shekhar Singh Rawal",
+    rollno: 32,
+    address: "Bhashi",
+    imgSrc:"./src/assets/shekhar.png",
+  },
+  {
+    name: "Yojana Pandey",
+    rollno: 38,
+    address: "Bhashi",
+    imgSrc:"./src/assets/yojana.jpg",
+  },
+];
 
-export default StudentsData
+export default StudentsData;
