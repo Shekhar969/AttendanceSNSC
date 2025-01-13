e6c4bb90bae685ffa6861c020df9135087742d6b
Shekhar969 <shekharrawal96@gmail.com>
2025-01-14 00:52:39 +0545

    Fully Working DB

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Cryptography from './Components/subjects/Cryptography.jsx'
-import DAA from './Components/subjects/Design-and-Analysis-of-Algorithms.jsx'
-import SAD from './Components/subjects/System-Analysis-and-Design.jsx'
-import WebTech from './Components/subjects/Web-Technology.jsx'
-import SM from './Components/subjects/Simulation-and-Modeling.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';  
+import './index.css';
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>
-  },
-  {
-    path: "/cryptography",
-    element: <Cryptography/>
-  },
-  {
-    path: "/DAA",
-    element: <DAA/>
-  },
-  {
-    path: "/SAD",
-    element: <SAD/>
-  },
-  {
-    path: "/WebTechnology",
-    element: <WebTech/>
-  },
-  {
-    path: "/SM",
-    element: <SM/>
-  },
-])
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <RouterProvider router={routes}/> */}
-    <RouterProvider router={routes} future={{ v7_startTransition: true }} />
-  </StrictMode>,
-)
+root.render(
+  <React.StrictMode>
+    <App />  
+  </React.StrictMode>
+);
